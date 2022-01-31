89fdb6008e51b406522925a2c464fa7dba5d9e33
Glorzo <thisaraeranga@gmail.com>
2022-01-30 21:29:59 -0600

    add search by topic feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event){
     event.preventDefault();
@@ -87,8 +88,34 @@ var getUserRepos = function(user) {
         });
 }
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
 
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data){
+                // console.log(data); (if you are confused about where did "items" came in the line below)
+                displayRepos(data.items, language);
+            })
+            
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+        
+    }).then();
+}
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    
+    if(language) {
+        getFeaturedRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent = "";
+    }
+}
 
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonEl.addEventListener("click", buttonClickHandler);
